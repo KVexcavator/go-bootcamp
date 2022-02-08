523fd01ded767e8163aad3974095222c610e72df
KVexcavator <str_token@mail.ru>
2022-02-08 16:53:15 +0300

    update liters prog & info

--- src/300_functions/liters.go
+++ src/300_functions/liters.go
@@ -3,15 +3,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
-func paintNeeded(width float64, height float64) {
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
 	area := width * height
-	fmt.Printf("%.2f liters needed\n", area/10.0)
+	return area / 10.0, nil
 }
 
 func main() {
-	paintNeeded(4.2, 3.0)
-	paintNeeded(5.2, 3.5)
-	paintNeeded(5.7, 6.3)
+	amount, err := paintNeeded(4.2, -3.0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%0.2f liters needed\n", amount)
 }
