920c704a069d94495613013972c3e4ab1d8600af
KVexcavator <str_token@mail.ru>
2022-03-08 15:53:58 +0300

    update average2 func

--- src/510_github.com/headfirstgo/average2/main.go
+++ src/510_github.com/headfirstgo/average2/main.go
@@ -8,19 +8,27 @@ import (
 	"strconv"
 )
 
+func average(numbers ...float64) float64 {
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum / float64(len(numbers))
+}
+
 func main() {
 	// fmt.Println(os.Args)     // [./average2 34.4 34.2 45.2 67.1]
 	// fmt.Println(os.Args[1:]) // [34.4 34.2 45.2 67.1]
 
 	arguments := os.Args[1:]
-	var sum float64 = 0
+	var numbers []float64
 	for _, argument := range arguments {
 		number, err := strconv.ParseFloat(argument, 64)
 		if err != nil {
 			log.Fatal(err)
 		}
-		sum += number
+		numbers = append(numbers, number)
 	}
-	sampleCount := float64(len(arguments))
-	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+	// При вызове функции с переменным количеством аргументов многоточие (...)
+	fmt.Printf("Average: %0.2f\n", average(numbers...))
 }
