54316633ae6d8a93d4975b1d3670c034c2718e60
KVexcavator <str_token@mail.ru>
2022-04-04 17:04:49 +0300

    finish info struct

--- src/820_github.com/kvexcavator/magazine/magazine.go
+++ src/820_github.com/kvexcavator/magazine/magazine.go
@@ -1,8 +1,24 @@
 // передача данных структуры между пакетами
 package magazine
 
+// здесь вызов полей адреса будет как HomeAddress.City
 type Subscriber struct {
+	Name        string
+	Rate        float64
+	Active      bool
+	HomeAddress Address
+}
+
+// здесь иожно напрямую обращаться к полям адреса(анонимность)
+type Employee struct {
 	Name   string
-	Rate   float64
-	Active bool
+	Salary float64
+	Address
+}
+
+type Address struct {
+	Street     string
+	City       string
+	State      string
+	PostalCode string
 }
--- src/830_export/export/main.go
+++ src/830_export/export/main.go
@@ -10,8 +10,24 @@ import (
 // имя структуры и полей в эспортируемой структуре должны начинаться с большой буквы
 func main() {
 	// в литерале все поля указывать не обязательно, не указанные будут с нулевым значением
-	subscriber := magazine.Subscriber{Name: "Aman Singh", Rate: 4.99, Active: true}
+	address := magazine.Address{Street: "123 Oak St", City: "Omaha", State: "NE", PostalCode: "68111"}
+	subscriber := magazine.Subscriber{Name: "Aman Singh"}
+	subscriber.HomeAddress = address
 	fmt.Println("Name:", subscriber.Name)
 	fmt.Println("Rate:", subscriber.Rate)
 	fmt.Println("Active:", subscriber.Active)
+	fmt.Println(subscriber.HomeAddress)
+	fmt.Println("Postal Code:", subscriber.HomeAddress.PostalCode)
+
+	// анонимный вызов
+	employee := magazine.Employee{Name: "Joy Carr"}
+	employee.Street = "456 Elm St"
+	employee.City = "Portland"
+	employee.State = "OR"
+	employee.PostalCode = "97222"
+	fmt.Println("Employee Name:", employee.Name)
+	fmt.Println("Street:", employee.Street)
+	fmt.Println("City:", employee.City)
+	fmt.Println("State:", employee.State)
+	fmt.Println("Postal Code:", employee.PostalCode)
 }
