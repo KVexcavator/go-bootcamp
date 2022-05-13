92f532c549fc0b05e88522da74c6f76b00e0cb0e
KVexcavator <str_token@mail.ru>
2022-05-13 17:12:17 +0300

    update info about input and args

---
+++ src/004_input/input.go
@@ -0,0 +1,11 @@
+// Программа илюстрирует пользовательский ввод
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Please give me your name: ")
+	var name string
+	fmt.Scanln(&name)
+	fmt.Println("Your name is", name)
+}
